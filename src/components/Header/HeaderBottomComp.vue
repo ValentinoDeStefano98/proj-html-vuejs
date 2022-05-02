ac0b333c7cdd60490521b0782bb82d466f3b7e59
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 10:26:25 +0200

    Header Bottom Section started

--- src/components/Header/HeaderBottomComp.vue
+++ src/components/Header/HeaderBottomComp.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <h4>HeaderBottom</h4>
+  <div class="container">
+    <div class="row">
+      <div class="col-2">
+        <img src="./../../assets/img/MasterStudy_logo.svg" alt="">
+      </div>
+      <div class="col-4 d-flex">
+        CATEGORY
+        <form class="d-flex">
+          <div class="mb-3">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div class="col-3">
+        <span>Became an insctructor</span>
+        <span>For Enterprise</span>
+      </div>
+      <div class="col-3">
+        <span>Log in</span>
+        <span>Sign up</span>
+        <span>icona</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +34,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.container{
+  width: 90%;
+  margin: 0 auto;
+}
 </style>
