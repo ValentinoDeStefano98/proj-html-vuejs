cba8c14345ed690bf5b7dade7c2e8dbc44f2b926
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-03 00:15:13 +0200

    Jumbo section completed

--- src/components/Main/Jumbo/JumboBottom.vue
+++ src/components/Main/Jumbo/JumboBottom.vue
@@ -1,92 +1,91 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-8">
-        <div>
+  <div class="container py-5">
+    <div class="row d-flex justify-content-between">
+      <div class="col-8 p-0">
           <img class="poster" src="./../../../assets/img/cat_1-770x375.jpg" alt="">
-        </div>
-        <div class="container p-0 m-0 w-100">
-          <div class="row row-cols">
+        <div class="container p-0 m-0 w-100 py-2">
+          <div class="row">
             <div class="col-6 text-start">
-              Software Development
+              <span class="fw-bold">Software Development</span>
             </div>
             <div class="col-6 text-end">
-              5 Courses
+              <span class="size">5 Courses</span> 
             </div>
           </div>
         </div>
       </div>
-      <div class="col-4 d-flex flex-column justify-content-between">
+      <div class="col-4 d-flex flex-column justify-content-beetwen pe-0">
         <div>
           <img class="poster" src="./../../../assets/img/cat_2-370x155.jpg" alt="">
         </div> 
-        <div class="container p-0 m-0 w-100">
+        <div class="container p-0 m-0 w-100 pt-1 pb-4">
           <div class="row row-cols">
             <div class="col-6 text-start">
-              Art
+              <span class="fw-bold">Art</span>
             </div>
             <div class="col-6 text-end">
-              8 Courses
+              <span class="size">8 Courses</span> 
             </div>
           </div>
         </div>
         <div>
           <img class="poster" src="./../../../assets/img/cat_3-370x155.jpg" alt="">
         </div>
-        <div class="container p-0 m-0 w-100">
-          <div class="row row-cols">
+        <div class="container p-0 m-0 w-100 pt-1">
+          <div class="row row-cols mt-1">
             <div class="col-6 text-start">
-              Material Design
+              <span class="fw-bold">Material Design</span>
             </div>
             <div class="col-6 text-end">
-              5 Courses
+              <span class="size">5 Courses</span> 
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="row row-cols-3">
-      <div class="col-4">
+
+    <div class="row">
+      <div class="col-4 ps-0">
         <div>
           <img class="poster" src="./../../../assets/img/cat_5-370x155.jpg" alt="">
         </div>
         <div class="container p-0 m-0 w-100">
-          <div class="row">
+          <div class="row pt-1">
             <div class="col-6 text-start">
-              Music
+              <span class="fw-bold">Music</span>
             </div>
             <div class="col-6 text-end">
-              5 Courses
+              <span class="size">5 Courses</span> 
             </div>
           </div>
         </div>
       </div>   
-      <div class="col-4">
+      <div class="col-4 px-1">
         <div>
           <img class="poster" src="./../../../assets/img/cat_4-370x155.jpg" alt="">
         </div>
         <div class="container p-0 m-0 w-100">
-          <div class="row">
+          <div class="row pt-1">
             <div class="col-6 text-start">
-              Exercise
+              <span class="fw-bold">Exercise</span>
             </div>
             <div class="col-6 text-end">
-              7 Courses
+              <span class="size">7 Courses</span> 
             </div>
           </div>
         </div>
       </div>   
-      <div class="col-4">
+      <div class="col-4 pe-0">
         <div>
           <img class="poster" src="./../../../assets/img/cat_6-370x155.jpg" alt="">
         </div>
         <div class="container p-0 m-0 w-100">
-          <div class="row">
+          <div class="row pt-1">
             <div class="col-6 text-start">
-              Photography
+              <span class="fw-bold">Photography</span> 
             </div>
             <div class="col-6 text-end">
-              4 Courses
+              <span class="size">4 Courses</span> 
             </div>
           </div>
         </div>
@@ -111,5 +110,9 @@ export default {
 
 .poster{
   max-width: 100%;
+}
+
+.size{
+  font-size: 0.9em;
 }
 </style>
--- src/components/Main/Jumbo/JumboMid.vue
+++ src/components/Main/Jumbo/JumboMid.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="container">
-    <div class="row row-cols-3">
-      <div class="col-4">
-        <h4>Worldwide recognize</h4>
-        <p>Our online course certificates can be used around the world and also in most popular universities and companies.</p>
+    <div class="row d-flex justify-content-between">
+      <div class="col-3 text-start minifont background py-2">
+        <div>
+          <i class="fa-solid fa-earth-americas fs-2 py-3"></i>
+          <h6>Worldwide recognize</h6>
+          <p>Our online course certificates can be used <br> around the world and also in most popular universities and companies.</p>
+        </div>
+        
       </div>
-      <div class="col-4">
-        <h4>Mostly Online Learning (sd)</h4>
-        <p>Masterstudy online certificates can be obtained in a range of specialized areas and typically take about a year to complete.</p>
+      <div class="col-3 text-start minifont background py-2">
+        <i class="fs-2 my-3 fa-solid fa-tv"></i>
+        <h6>Mostly Online Learning (sd)</h6>
+        <p>Masterstudy online certificates can be <br> obtained in a range of specialized areas and typically take about a year to complete.</p>
       </div>
-      <div class="col-4">
-        <h4>Graduate in as little as 1 Year</h4>
-        <p>Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy.</p>
+      <div class="col-3 text-start minifont background py-2">
+        <i class="fa-solid fa-calendar-check fs-2 my-3"></i>
+        <h6>Graduate in as little as 1 Year</h6>
+        <p>Online post-graduate certificates are a popular <br> way to develop your professional <br> qualifications of Masterstudy.</p>
       </div>
     </div>
   </div>
@@ -28,5 +34,13 @@ export default {
 .container{
   width: 65%;
   margin: 0 auto;
+}
+
+.minifont{
+  font-size: 0.8em;
+}
+
+.background{
+  background-color: #e4e8ed;
 }
 </style>
