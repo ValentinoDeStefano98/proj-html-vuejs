e20932e90d64e2a2d12de0cbd84792e8c5af1421
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 21:44:16 +0200

    Popular courses section started

--- src/components/Main/PopularCoursesComp.vue
+++ src/components/Main/PopularCoursesComp.vue
@@ -1,6 +1,150 @@
 <template>
   <div>
-    <h4>Popular Courses Section</h4>
+    <div class="big-container">
+      <h2>Popular Courses</h2>
+      <p>Discover our most popular courses for self lerning</p>
+      <div class="container">
+        <div class="row py-4">
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card">
+              <img src="./../../assets/img/photo-1461749280684-dccba630e2f6-544x322.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <div class="text-start">
+                  <span>Apache ></span>               
+                <h6 class="card-title">Web Coding and Apache Basics</h6>
+                </div>
+                <hr>
+                <div class="container p-0 m-0">
+                  <div class="row">
+                    <div class="col-6 text-start">
+                      <span>6 hours</span>
+                    </div>
+                    <div class="col-6 text-end">
+                      <span>Free</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <div>
+        <span>freccia</span>
+        <span>freccia</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +156,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.big-container{
+  background-color: #F0F4F9;
+}
 </style>
