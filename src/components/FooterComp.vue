08ee1a2f8a9e5e54c3ca120bdf10148022856958
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 22:19:42 +0200

    Footer Section added

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,60 @@
 <template>
-  <div>
-    <h2>Footer</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <h4>About</h4>
+        <p>MasterStudy is Education WordPress theme featured by Learning Management System (LMS) for online education. Developed by SystemThemes.</p>
+      </div>
+      <div class="col-3">
+        <h4>Contact</h4>
+        <p>USA, California 20, First Avenue, California</p>
+        <br>
+        <span>Tel: +1 212 458 300 32</span>
+        <br> 
+        <span>Fax: +1 212 375 24 414</span>
+        <br>
+        <span>info@masterstudy.com</span>
+      </div>
+      <div class="col-3">
+        <h4>Pages</h4>
+        <div class="d-flex">
+          <ul>
+            <li>Blog</li>
+            <li>Home</li>
+            <li>Shortcodes</li>
+          </ul>
+          <ul>
+            <li>Blog</li>
+            <li>Home</li>
+            <li>Shortcodes</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-3">
+        <h4>Blog</h4>
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <img src="./../assets/img/photo-1517520287167-4bbf64a00d66-50x50.jpeg" alt="">
+            </div>
+            <div class="col-8">
+              <p>Our main target is to "Developing Yourself as a Leader"</p>
+              <span>-August 9, 2018</span>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-4">
+              <img src="./../assets/img/photo-1490376840453-5f616fbebe5b-50x50.jpeg" alt="">
+            </div>
+            <div class="col-8">
+              <p>Those Other College Expenses You Aren't Thinking About</p>
+              <span>-June 3, 2015</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
