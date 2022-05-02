74dc9ac52fd3bd9f4063eb998ab0710d33bb9cc0
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 17:15:53 +0200

    Read More Section started

--- src/components/Main/ReadMoreComp.vue
+++ src/components/Main/ReadMoreComp.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <h4>Read More Section</h4>
+  <div class="big-container py-5">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-6">
+          <h2>Limitless learning, more possibilities</h2>
+          <p>Online courses open the opportunity for learning to almost anyone regardless of their scheduling commitments.</p>
+          <button>Read more</button>
+        </div>
+        <div class="col-6">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +22,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.big-container{
+  width: 100%;
+  background-image: url("./../../assets/img/Untitled-1-1-1-1-1.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-x: 60%;
+  background-position-y: 60%;
+}
 
+.container{
+  width: 65%;
+  margin: 0 auto;
+}
 </style>
