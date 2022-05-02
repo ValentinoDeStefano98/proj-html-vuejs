46f6c6674c538d8e66adfab8e1f2cc0175bc9160
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 21:52:06 +0200

    Offers section started

--- src/components/Main/OffersComp.vue
+++ src/components/Main/OffersComp.vue
@@ -1,6 +1,35 @@
 <template>
-  <div>
-    <h4>Offers Section</h4>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="container border">
+          <div class="row">
+            <div class="col-4">
+              <img src="./../../assets/img/image_box_1-221x231.png" alt="">
+            </div>
+            <div class="col-8">
+              <h3>Became an instructor</h3>
+              <p>Teach what you love. Masterstudy gives you the tools to create a course.</p>
+              <button>Start teaching</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="container border">
+          <div class="row">
+            <div class="col-4">
+              <img src="./../../assets/img/image_box_2-234x231.png" alt="">
+            </div>
+            <div class="col-8">
+              <h3>Setup for business</h3>
+              <p>Get unlimited acces to 2,500 of Udemy's top courses for your team.</p>
+              <button>Doing business</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
