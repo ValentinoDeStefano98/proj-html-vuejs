68e5af6b17dd50ab7f3353285fcb858eda77a401
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 22:00:06 +0200

    Pre-footer section started

--- src/components/Main/PreFooterComp.vue
+++ src/components/Main/PreFooterComp.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h4>Pre-Footer Section</h4>
+  <div class="big-container">
+    <div class="container py-5">
+      <div class="row py-5">
+        <div class="col-8 bg-white py-5 my-4">
+          <h3>Come as you are</h3>
+          <p>MasterStudy used is an excellent workshop whether you come as a consuelor, advisor, administrator, or faculty member. I am going home empowered. I am looking forward to attending the On Course MasterStudy and the MasterStudy national conference next year and facilitating the use of this excellent retention/student success course at my college</p>
+          <p>Tim Sab <br> General Developer, Stylemix Themes</p>
+        </div>
+        <div class="4">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +23,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.big-container{
+  width: 100%;
+  background-image: url("./../../assets/img/brooke-cagle-224821-unsplash-1-1920x700.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-x: 60%;
+  background-position-y: 60%;
+}
 </style>
