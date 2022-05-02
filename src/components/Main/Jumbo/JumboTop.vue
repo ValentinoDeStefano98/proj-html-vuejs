1203ddd15ca25deaa7440c051821faba17637f98
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 10:39:07 +0200

    Jumbo top section added

--- src/components/Main/Jumbo/JumboTop.vue
+++ src/components/Main/Jumbo/JumboTop.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h6>JumboTop</h6>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <h2>Start Investing In You</h2>
+        <p>With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you</p>
+        <button>Join for free</button>
+      </div>
+      <div class="col-4">
+        <img class="base" src="./../../../assets/img/base.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +21,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.container{
+  width: 65%;
+  margin: 0 auto;
+}
 
+.base{
+  max-width: 100%;
+}
 </style>
