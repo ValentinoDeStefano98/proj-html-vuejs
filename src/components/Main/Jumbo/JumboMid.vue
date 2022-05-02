c7ab1f1d8250c39c6d281cb301a27016d99ca5e9
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-02 10:47:52 +0200

    Jumbo mid section added

--- src/components/Main/Jumbo/JumboMid.vue
+++ src/components/Main/Jumbo/JumboMid.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h6>JumboMid</h6>
+  <div class="container">
+    <div class="row row-cols-3">
+      <div class="col-4">
+        <h4>Worldwide recognize</h4>
+        <p>Our online course certificates can be used around the world and also in most popular universities and companies.</p>
+      </div>
+      <div class="col-4">
+        <h4>Mostly Online Learning (sd)</h4>
+        <p>Masterstudy online certificates can be obtained in a range of specialized areas and typically take about a year to complete.</p>
+      </div>
+      <div class="col-4">
+        <h4>Graduate in as little as 1 Year</h4>
+        <p>Online post-graduate certificates are a popular way to develop your professional qualifications of Masterstudy.</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +25,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.container{
+  width: 65%;
+  margin: 0 auto;
+}
 </style>
